52c9784348842a2990a7802a7b83fc32432264e7
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-13 20:28:25 +0200

    Begin implementing static utility methods in Grid

--- src/Model/Data/CoordSystem/Grid.java
+++ src/Model/Data/CoordSystem/Grid.java
@@ -1,8 +1,40 @@
 package src.Model.Data.CoordSystem;
 
+import src.Model.Data.LinkedList.List;
+
 public class Grid
 {
     private static Vector3 origin = Vector3.zero;
 
-    // TODO: Later Implement Interface for Coordinate View Window
+    public static double getPathLength(List<Vector2> path)
+    {
+        double length = 0;
+        Vector2 prevPoint = null;
+        for (Vector2 point: path)
+        {
+            if (prevPoint == null)
+                prevPoint = point;
+            length += prevPoint.distance(point);
+            prevPoint = point;
+        }
+        return length;
+    }
+
+    public static int[] getNNearest(Vector2 startPoint, List<Vector2> otherPoints, int number)
+    {
+        if (number > otherPoints.count())
+            throw new IllegalArgumentException();
+
+        double[] distances = new double[otherPoints.count()];
+        int index = 0;
+        for (Vector2 point: otherPoints)
+            distances[index] = startPoint.distance(point);
+
+        int[] indices = new int[number];
+        double minimum = Integer.MAX_VALUE;
+        for (int i = 0; i < distances.length; i++)
+            minimum = Math.min(minimum, distances[i]);
+
+
+    }
 }
