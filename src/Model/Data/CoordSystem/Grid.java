30f4c1788df56a62776340b09366813fdf8196ca
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-14 00:59:43 +0200

    Updated Grid

--- src/Model/Data/CoordSystem/Grid.java
+++ src/Model/Data/CoordSystem/Grid.java
@@ -25,16 +25,46 @@ public class Grid
         if (number > otherPoints.count())
             throw new IllegalArgumentException();
 
+        if (number == 0 || otherPoints.count() == 0)
+            return new int[0];
+
         double[] distances = new double[otherPoints.count()];
         int index = 0;
         for (Vector2 point: otherPoints)
+        {
             distances[index] = startPoint.distance(point);
+            index++;
+        }
 
         int[] indices = new int[number];
-        double minimum = Integer.MAX_VALUE;
-        for (int i = 0; i < distances.length; i++)
-            minimum = Math.min(minimum, distances[i]);
+        double minimum = Double.MAX_VALUE;
+        for (double distance : distances)
+            minimum = Math.min(minimum, distance);
 
-
+        /*
+         * Wenn man von 5 gesuchten Elementen ausgeht, dann
+         * ist dieses Vorgehen ab ca. 150 Eingabeelementen
+         * schneller als das vorherige Sortieren der Elemente
+         * in O(n * log(n)) Zeit
+         */
+        index = 0;
+        double newMinimum = Double.MAX_VALUE;
+        for (int i = 0; i < indices.length; i++)
+        {
+            for (int j = 0; j < distances.length; j++)
+            {
+                if (minimum == distances[j])
+                {
+                    indices[index] = j;
+                    index++;
+                }
+                else if (minimum < distances[j])
+                {
+                    newMinimum = Math.min(newMinimum, distances[j]);
+                }
+            }
+            minimum = newMinimum;
+        }
+        return indices;
     }
 }
