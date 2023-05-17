5d5ab4ca82437957b101bcc581ddf6ea0a817387
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-17 21:26:05 +0200

    Added indexOf and edge overload methods to Graph

--- src/Model/Data/Graph/Graph.java
+++ src/Model/Data/Graph/Graph.java
@@ -2,6 +2,8 @@ package src.Model.Data.Graph;
 
 import src.Model.Data.LinkedList.List;
 
+import java.util.function.BiPredicate;
+
 /**
  * Generischer Graph
  * @param <T> Beliebiger Datentyp T
@@ -125,5 +127,46 @@ public class Graph<T>
             index1++;
         }
         return weights;
+    }
+
+    /**
+     * Methode zum Ausgeben des Index eines Objektes
+     * @param object Objekt des Typen T
+     * @param equality Benutzer spezifizierte Funktion zum Überprüfen
+     *                 der Gleichheit zweier Objekte
+     * @return Index des Objektes
+     */
+    public int indexOf(T object, BiPredicate<T, T> equality)
+    {
+        return nodes.indexOf(new Node<>(object), (Node<T> a, Node<T> b) -> equality.test(a.getData(), b.getData()));
+    }
+
+    /**
+     * Methode zum Hinzufügen eines Kantenwertes im Graphen
+     * @param obj1 Objekt des Typen T
+     * @param obj2 Objekt des Typen T
+     * @param weight Wert der Kante als Double
+     * @param equality Benutzer spezifizierte Funktion zum Überprüfen
+     *                 der Gleichheit zweier Objekte
+     */
+    public void setEdge(T obj1, T obj2, double weight, BiPredicate<T, T> equality)
+    {
+        int i1 = indexOf(obj1, equality), i2 = indexOf(obj2, equality);
+        matrix.get(i1).set(i2, weight);
+        matrix.get(i2).set(i1, weight);
+    }
+
+    /**
+     * Methode zum Ausgeben eines Kantenwertes im Graphen
+     * @param obj1 Objekt des Typen T
+     * @param obj2 Objekt des Typen T
+     * @param equality Benutzer spezifizierte Funktion zum Überprüfen
+     *                 der Gleichheit zweier Objekte
+     * @return Wert der Kante als Double
+     */
+    public double getEdge(T obj1, T obj2, BiPredicate<T, T> equality)
+    {
+        int i1 = indexOf(obj1, equality), i2 = indexOf(obj2, equality);
+        return matrix.get(i1).get(i2);
     }
 }
