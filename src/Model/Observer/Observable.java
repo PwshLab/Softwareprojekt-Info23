4c101db6a8880f1a8d0d5b77d0bf616c49452026
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-17 20:41:13 +0200

    Added constructor to Observable

--- src/Model/Observer/Observable.java
+++ src/Model/Observer/Observable.java
@@ -6,6 +6,11 @@ public class Observable
 {
     private List<Observer> observers;
 
+    public Observable()
+    {
+        observers = new List<>();
+    }
+
     protected void notifyObservers()
     {
         for (Observer observer: observers)
