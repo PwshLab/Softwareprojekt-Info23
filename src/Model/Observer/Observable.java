469617d4d5949332bbf564d1ba66b181f6ac9433
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-17 20:38:27 +0200

    Added Observer and Observable classes

---
+++ src/Model/Observer/Observable.java
@@ -0,0 +1,26 @@
+package src.Model.Observer;
+
+import src.Model.Data.LinkedList.List;
+
+public class Observable
+{
+    private List<Observer> observers;
+
+    protected void notifyObservers()
+    {
+        for (Observer observer: observers)
+        {
+            observer.update();
+        }
+    }
+
+    protected void addObserver(Observer observer)
+    {
+        observers.add(observer);
+    }
+
+    protected void removeObserver(Observer observer)
+    {
+        observers.remove(observer, Object::equals);
+    }
+}
---
+++ src/Model/Observer/Observer.java
@@ -0,0 +1,6 @@
+package src.Model.Observer;
+
+public interface Observer
+{
+    public void update();
+}
