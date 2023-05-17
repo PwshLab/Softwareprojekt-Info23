be5d563b5aa069b08eec8ce8736fd7da4b972b34
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-17 20:38:40 +0200

    Started implementing Model

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,5 +1,33 @@
 package src.Model;
 
-public class Model
+import src.Model.Data.Graph.Graph;
+import src.Model.Data.LinkedList.List;
+import src.Model.Data.LinkedList.SortedList;
+import src.Model.Data.Locality.Locality;
+import src.Model.Data.Locality.LocalityOrder;
+import src.Model.Observer.Observable;
+
+public class Model extends Observable
 {
+    private Graph<Locality> graph;
+
+    public List<Locality> getElements()
+    {
+        return graph.getElements();
+    }
+
+    public List<Locality> getElements(int order)
+    {
+        List<Locality> elements = getElements();
+
+        switch (order)
+        {
+            case 1:
+                return new SortedList<>(elements, LocalityOrder::ByName);
+            case 2:
+                return new SortedList<>(elements, LocalityOrder::ByType);
+            default:
+                return elements;
+        }
+    }
 }
