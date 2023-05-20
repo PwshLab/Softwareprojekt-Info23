01006789acd7984c1bb5db6aa00109d976a0c3c0
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-20 15:48:44 +0200

    Begin implementing observer mechanism in View

--- src/View/View.java
+++ src/View/View.java
@@ -1,5 +1,24 @@
 package src.View;
 
+import src.Model.Model;
+import src.View.Screen.ListView.ListView;
+import src.View.Window.Navigation;
+
 public class View
 {
+    private Model model;
+
+    private Navigation navigation;
+
+    public View(Model model)
+    {
+        this.model = model;
+        navigation = new Navigation();
+    }
+
+    private void registerViews()
+    {
+        model.addObserver(navigation);
+    }
+
 }
