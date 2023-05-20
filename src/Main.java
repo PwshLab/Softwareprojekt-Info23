7b1371be1f256d0e54f551774d3c7a8c7dc4029b
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-20 15:49:10 +0200

    Updated entrypoint

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ package src;
 import src.Controller.Controller;
 import src.Model.Model;
 import src.View.View;
+import src.View.Window.Navigation;
 
 public class Main {
 
     public static void main(String[] args)
     {
+        /*
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller();
+        */
+        Navigation nav = new Navigation();
     }
 }
