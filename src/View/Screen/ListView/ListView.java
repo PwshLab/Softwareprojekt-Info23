f44b0df438c8a8a064eedf92bfc2da97f1702efb
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-24 18:59:27 +0200

    Updates

---
+++ src/View/Screen/ListView/ListView.java
@@ -0,0 +1,61 @@
+package src.View.Screen.ListView;
+
+import src.Model.Data.LinkedList.List;
+import src.Model.Data.LinkedList.SortedList;
+import src.Model.Data.Locality.Locality;
+import src.Model.Data.Locality.LocalityOrder;
+import src.Model.Model;
+import src.Model.Observer.Observer;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.*;
+
+public class ListView extends JPanel
+{
+    private JTable table;
+    private RowSorter<ListTableModel> rowSorter;
+    private int currentOrder;
+    private String currentSearch;
+
+    public ListView(Model model)
+    {
+        this.currentOrder = 1;
+        this.currentSearch = "";
+
+        initialize();
+        table.setModel(new ListTableModel(model));
+    }
+
+    private void initialize()
+    {
+        setOpaque(true);
+        setVisible(true);
+
+        table = new JTable();
+        table.setPreferredScrollableViewportSize(new Dimension(500, 300));
+        table.setFillsViewportHeight(true);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.setRowSelectionAllowed(false);
+        table.setColumnSelectionAllowed(false);
+        table.setCellSelectionEnabled(false);
+        table.setDragEnabled(false);
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane);
+    }
+
+    private void initializeSorter()
+    {
+        rowSorter = new TableRowSorter<>();
+        table.setRowSorter(rowSorter);
+    }
+
+    //TODO : Implement searching and sorting by attributes
+    //TODO : Add information block for selected entry
+    public void displayData()
+    {
+
+    }
+}
