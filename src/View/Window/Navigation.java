c8176f02f9bd0a7a59206e48c85190fca8479c3d
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-24 20:42:29 +0200

    Added getter methods to Navigation

--- src/View/Window/Navigation.java
+++ src/View/Window/Navigation.java
@@ -1,10 +1,6 @@
 package src.View.Window;
 
-import src.Model.Data.LinkedList.List;
-import src.Model.Data.Locality.Locality;
-import src.Model.Data.Value.Pair;
 import src.Model.Model;
-import src.Model.Observer.Observer;
 import src.View.Screen.ListView.ListView;
 import src.View.Screen.MapView;
 
@@ -12,7 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class Navigation extends JPanel implements Observer
+public class Navigation extends JPanel
 {
     private final Model model;
     private ListView listView;
@@ -23,7 +19,6 @@ public class Navigation extends JPanel implements Observer
         this.model = model;
 
         initializeNavigationGUI();
-        update();
     }
 
     public void initializeNavigationGUI()
@@ -32,18 +27,15 @@ public class Navigation extends JPanel implements Observer
         add(mainPanel);
 
         mainPanel.setPreferredSize(new Dimension(1200, 600));
-        //mainPanel.setBackground(Color.RED);
         mainPanel.setLayout(new GridLayout());
 
         JPanel panelLeft = new JPanel();
         panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
-        //panelLeft.setBackground(Color.GREEN);
         panelLeft.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         mainPanel.add(panelLeft);
 
         JPanel panelRight = new JPanel();
         panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.Y_AXIS));
-        //panelRight.setBackground(Color.CYAN);
         panelRight.setBorder(BorderFactory.createEmptyBorder(25, 25, 50, 25));
         mainPanel.add(panelRight);
 
@@ -56,12 +48,13 @@ public class Navigation extends JPanel implements Observer
         setVisible(true);
     }
 
-    public void update()
+    public ListView getListView()
     {
-        List<Locality> localities = model.getElements();
-        @SuppressWarnings("unchecked") Class<Pair<Integer, Integer>> pairClass = (Class<Pair<Integer, Integer>>)(Class<?>)Pair.class;
-        Pair<Integer, Integer>[] edges = model.getEdges().toArray(pairClass);
-        Integer[] lastPath = model.getLastPath().toArray(Integer.class);
-        mapView.displayData(localities.toArray(Locality.class), edges, lastPath);
+        return listView;
+    }
+
+    public MapView getMapView()
+    {
+        return mapView;
     }
 }
