829e494113b1007adcb1d41f235834934212d818
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-24 18:59:36 +0200

    Added example data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 package src;
 
 import src.Controller.Controller;
+import src.Model.Data.CoordSystem.Vector2;
+import src.Model.Data.LinkedList.List;
+import src.Model.Data.Locality.Locality;
+import src.Model.Data.Locality.LocalityType;
 import src.Model.Model;
 import src.View.View;
 
@@ -11,5 +15,13 @@ public class Main {
         Model model = new Model();
         View view = new View(model);
         Controller controller = new Controller(model, view);
+
+        addData(model);
+    }
+
+    private static void addData(Model model)
+    {
+        Locality locality = new Locality("Test", LocalityType.BAR, new Vector2(0, 0));
+        model.addElement(locality, new List<>());
     }
 }
