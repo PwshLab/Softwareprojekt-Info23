50142db752f4e7729f5c30d92bb3ae9602fae176
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-24 15:05:40 +0200

    Removed UI debug colors

--- src/View/Window/Navigation.java
+++ src/View/Window/Navigation.java
@@ -27,43 +27,31 @@ public class Navigation extends JPanel implements Observer
 
     public void initializeNavigationGUI()
     {
-        //frame = new JFrame("Navigation");
-        //frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
         JPanel mainPanel = new JPanel();
         add(mainPanel);
 
         mainPanel.setPreferredSize(new Dimension(1200, 600));
-        mainPanel.setBackground(Color.RED);
+        //mainPanel.setBackground(Color.RED);
         mainPanel.setLayout(new GridLayout());
 
         JPanel panelLeft = new JPanel();
         panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
-        panelLeft.setBackground(Color.GREEN);
+        //panelLeft.setBackground(Color.GREEN);
         panelLeft.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         mainPanel.add(panelLeft);
 
         JPanel panelRight = new JPanel();
         panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.Y_AXIS));
-        panelRight.setBackground(Color.CYAN);
+        //panelRight.setBackground(Color.CYAN);
         panelRight.setBorder(BorderFactory.createEmptyBorder(25, 25, 50, 25));
         mainPanel.add(panelRight);
 
-        //JLabel emptyLabel = new JLabel("Lorem Ipsum Dolor Sit Amet");
-        //emptyLabel.setPreferredSize(new Dimension(1200, 600));
-        //panelRight.add(emptyLabel, BorderLayout.CENTER);
-
         listView = new ListView();
         panelLeft.add(listView);
-        //frame.getContentPane().add(listView, BorderLayout.WEST);
-        //emptyLabel.add(listView, BorderLayout.WEST);
-        //frame.setContentPane(listView);
 
         mapView = new MapView(400, 400);
-        //mapView.setBackground(Color.BLUE);
         panelRight.add(mapView);
 
-        //pack();
         setVisible(true);
     }
 
