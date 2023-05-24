b6f3e41d3bcb0e91ca879ae239a0dd1f8b770f6f
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-05-24 18:57:02 +0200

    Extended Vector2 with additional methods

--- src/Model/Data/CoordSystem/Vector2.java
+++ src/Model/Data/CoordSystem/Vector2.java
@@ -18,6 +18,54 @@ public class Vector2 extends Vector3
     }
 
     /**
+     * Methode zum Addieren zweier Vektoren
+     * @param other Anderer Vektor
+     * @return Resultierender Vektor
+     */
+    public Vector2 add(Vector2 other)
+    {
+        return new Vector2(this.x1 + other.x1, this.x2 + other.x2);
+    }
+
+    /**
+     * Methode für den Gegenvektor eines Vektors
+     * @return Gegenvektor des Vektors
+     */
+    public Vector2 neg()
+    {
+        return new Vector2(-this.x1, -this.x2);
+    }
+
+    /**
+     * Methode zum Subtrahieren zweier Vektoren
+     * @param other Anderer Vektor
+     * @return Resultierender Vektor
+     */
+    public Vector2 sub(Vector2 other)
+    {
+        return this.add(other.neg());
+    }
+
+    /**
+     * Methode zum Skalieren eines Vektors
+     * @param n Skalarfaktor
+     * @return Resultierender Vektor
+     */
+    public Vector2 multiply(double n)
+    {
+        return new Vector2(this.x1 * n, this.x2 * n);
+    }
+
+    /**
+     * Methode zum Berechnen des Normalvektors eines Vektors
+     * @return Normalvektor des Vektors
+     */
+    public Vector2 normalized()
+    {
+        return this.multiply(1 / this.magnitude());
+    }
+
+    /**
      * Methode zum Ausgeben eines Vektors als String
      * @return Vektor als String
      */
