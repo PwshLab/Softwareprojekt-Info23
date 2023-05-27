e48da4a51232ef14e204a6482d9f253bf031a62e
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-28 00:13:15 +0200

    Added input fields to SearchFilter

--- src/Controller/Navigation/SearchFilter.java
+++ src/Controller/Navigation/SearchFilter.java
@@ -1,10 +1,16 @@
 package src.Controller.Navigation;
 
+import src.Controller.Component.NumberField;
+import src.Controller.Component.PointField;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class SearchFilter extends JPanel
 {
+    private final PointField pointField;
+    private final NumberField numberField;
+
     public SearchFilter()
     {
         setOpaque(true);
@@ -14,5 +20,20 @@ public class SearchFilter extends JPanel
 
         JLabel label = new JLabel("Suche Begrenzen");
         add(label, BorderLayout.NORTH);
+
+        JPanel centerPanel = new JPanel();
+        JPanel upperCenter = new JPanel();
+        centerPanel.setLayout(new GridLayout(2, 1));
+        centerPanel.add(upperCenter);
+        upperCenter.setLayout(new BoxLayout(upperCenter, BoxLayout.X_AXIS));
+        pointField = new PointField("Position");
+        upperCenter.add(pointField);
+        numberField = new NumberField("Entfernung");
+        upperCenter.add(numberField);
+        add(centerPanel, BorderLayout.CENTER);
+
+        JButton button = new JButton("Anwenden");
+        button.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        centerPanel.add(button);
     }
 }
