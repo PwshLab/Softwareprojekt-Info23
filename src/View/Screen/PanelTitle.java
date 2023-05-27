1e173f8aa1bf63b9282450692ea23fd989920520
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-27 20:10:33 +0200

    Implemented PanelTitle for repeatable titles

---
+++ src/View/Screen/PanelTitle.java
@@ -0,0 +1,19 @@
+package src.View.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelTitle extends JPanel
+{
+    public PanelTitle(String titleString)
+    {
+        setOpaque(true);
+        setMaximumSize(new Dimension(1200, 5));
+        setBorder(BorderFactory.createBevelBorder(1));
+
+        JLabel label = new JLabel(titleString);
+        Font labelFont = new Font(label.getFont().getName(), 0, 20);
+        label.setFont(labelFont);
+        add(label);
+    }
+}
