c0b59224bcac53907ecd56c8b4898c6fab413c08
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-27 20:10:56 +0200

    Begin implementing SearchFilter

--- src/Controller/Navigation/SearchFilter.java
+++ src/Controller/Navigation/SearchFilter.java
@@ -1,11 +1,18 @@
 package src.Controller.Navigation;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SearchFilter extends JPanel
 {
     public SearchFilter()
     {
+        setOpaque(true);
+        setVisible(true);
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
 
+        JLabel label = new JLabel("Suche Begrenzen");
+        add(label, BorderLayout.NORTH);
     }
 }
