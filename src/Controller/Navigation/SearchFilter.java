66b9fd3a7e612425b76b87dcbc3cc1df6066e532
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-28 08:09:02 +0200

    Added TODO

--- src/Controller/Navigation/SearchFilter.java
+++ src/Controller/Navigation/SearchFilter.java
@@ -47,7 +47,7 @@ public class SearchFilter extends JPanel implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        // TODO : Fix mistakes in error checking
+        // TODO: Fix mistakes in error checking
         if (pointField.hasError() || numberField.hasError())
         {
             model.resetFilterByDistance();
