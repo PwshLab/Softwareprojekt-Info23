7a3154e9f35acfdbc643cb507a31b137358f12da
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-28 09:49:17 +0200

    Implemented Whole Number dataField

---
+++ src/Controller/Component/WholeNumberField.java
@@ -0,0 +1,28 @@
+package src.Controller.Component;
+
+public class WholeNumberField extends TextDataField<Integer>
+{
+    public WholeNumberField(String fieldLabel)
+    {
+        super(fieldLabel);
+    }
+
+    @Override
+    protected boolean checkError()
+    {
+        try
+        {
+            Integer.parseInt(textField.getText());
+            return false;
+        } catch (NumberFormatException e)
+        {
+            return true;
+        }
+    }
+
+    @Override
+    protected Integer parseData()
+    {
+        return Integer.parseInt(textField.getText());
+    }
+}
