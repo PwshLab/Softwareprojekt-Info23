31b28e34fab2185aa638fdcef55127f2311fa203
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-30 17:47:12 +0200

    Implement AbortHandler for cancelling an operation Add ability to set confirmation action

--- src/Controller/Administration/Mode/ConfirmMode.java
+++ src/Controller/Administration/Mode/ConfirmMode.java
@@ -4,9 +4,14 @@ import src.Controller.Administration.EditingMode;
 import src.Controller.Administration.EditingPanel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConfirmMode extends PanelMode
 {
+    private Runnable confirmAction;
+
     public ConfirmMode(EditingPanel editingPanel)
     {
         super(editingPanel, "Bestätigen");
@@ -17,6 +22,14 @@ public class ConfirmMode extends PanelMode
     protected void initializeForm(JPanel formPanel)
     {
 
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.WHITE);
+        JButton submitButton = new JButton("Abbrechen");
+        submitButton.addActionListener(new AbortHandler(editingPanel));
+        submitButton.setMaximumSize(new Dimension(100, 50));
+        buttonPanel.add(submitButton);
+        formPanel.add(buttonPanel, BorderLayout.SOUTH);
     }
 
     @Override
@@ -25,5 +38,24 @@ public class ConfirmMode extends PanelMode
 
     }
 
+    public void setConfirmAction(Runnable confirmAction)
+    {
+        this.confirmAction = confirmAction;
+    }
 
+    private class AbortHandler implements ActionListener
+    {
+        private EditingPanel editingPanel;
+
+        public AbortHandler(EditingPanel editingPanel)
+        {
+            this.editingPanel = editingPanel;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            editingPanel.setEditingMode(EditingMode.SELECT);
+        }
+    }
 }
