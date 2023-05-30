bcfbe9b57fd1e24f09bc85307df63f849f02252d
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-30 17:09:14 +0200

    Implemented DeleteMode panel Added TODO

--- src/Controller/Administration/Mode/DeleteMode.java
+++ src/Controller/Administration/Mode/DeleteMode.java
@@ -2,11 +2,15 @@ package src.Controller.Administration.Mode;
 
 import src.Controller.Administration.EditingMode;
 import src.Controller.Administration.EditingPanel;
+import src.Controller.Component.WholeNumberField;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DeleteMode extends PanelMode
 {
+    private WholeNumberField numberField;
+
     public DeleteMode(EditingPanel editingPanel)
     {
         super(editingPanel, "Entfernen");
@@ -16,13 +20,22 @@ public class DeleteMode extends PanelMode
     @Override
     protected void initializeForm(JPanel formPanel)
     {
-
+        formPanel.setLayout(new BorderLayout());
+        formPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        numberField = new WholeNumberField("Index der Lokalität");
+        numberField.setPreferredSize(new Dimension(150, 25));
+        formPanel.add(numberField);
     }
 
+    // TODO: Add confirmation panel integration
     @Override
     protected void handleSubmit()
     {
-
+        if (!numberField.hasError())
+            if (editingPanel.checkIndexBounds(numberField.readData()))
+                editingPanel.removeLocality(numberField.readData());
+            else
+                numberField.notifyError();
     }
 
 
