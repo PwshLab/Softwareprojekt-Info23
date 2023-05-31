8bee27348b55660eb9b667dd8092efc883b71034
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-31 09:59:53 +0200

    Begin implementing LoginPrompt

--- src/Controller/WindowSwitch/LoginPrompt.java
+++ src/Controller/WindowSwitch/LoginPrompt.java
@@ -1,8 +1,41 @@
 package src.Controller.WindowSwitch;
 
+import src.Controller.Controller;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginPrompt extends JWindow
+public class LoginPrompt implements ActionListener
 {
+    private final Controller controller;
+    private final JPanel loginPanel;
+    private JButton loginButton;
 
+    public LoginPrompt(Controller controller)
+    {
+        this.controller = controller;
+
+        JFrame loginFrame = new JFrame("Anmeldung");
+        loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginFrame.setResizable(false);
+        loginFrame.setVisible(true);
+        loginPanel = new JPanel();
+        loginFrame.setContentPane(loginPanel);
+        loginFrame.pack();
+
+        initialize();
+    }
+
+    private void initialize()
+    {
+        loginPanel.setPreferredSize(new Dimension(200, 150));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
 }
