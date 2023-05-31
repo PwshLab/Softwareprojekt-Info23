9642d70ce32e01f5db7593e37a98f48db890b189
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-31 15:45:17 +0200

    Fixed LoginPrompt layout problem Made LoginPrompt hide on close Removed TODO

--- src/Controller/WindowSwitch/LoginPrompt.java
+++ src/Controller/WindowSwitch/LoginPrompt.java
@@ -12,8 +12,8 @@ import java.awt.event.WindowListener;
 public class LoginPrompt implements ActionListener
 {
     private final Controller controller;
+    private final JFrame loginFrame;
     private final JPanel loginPanel;
-    private JButton loginButton;
     private TextField loginField;
 
     private static final String loginPassword = "Passwort1234";
@@ -22,27 +22,26 @@ public class LoginPrompt implements ActionListener
     {
         this.controller = controller;
 
-        JFrame loginFrame = new JFrame("Anmeldung");
+        loginFrame = new JFrame("Anmeldung");
         loginFrame.setResizable(false);
         loginFrame.setVisible(true);
         loginPanel = new JPanel();
         loginFrame.setContentPane(loginPanel);
 
-        // TODO: Add custom window listener that only closes login window
-        //loginFrame.addWindowListener();
         // TODO: Make frame appear in the middle of current window
 
         initialize();
 
         loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
     }
 
     private void initialize()
     {
         loginPanel.setLayout(new BorderLayout());
-        //loginPanel.setPreferredSize(new Dimension(200, 150));
         loginPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         loginField = new TextField("Passwort", true);
+        loginField.addActionListener(this);
         loginPanel.add(loginField, BorderLayout.CENTER);
         JPanel buttonPanel = new JPanel();
         JButton button = new JButton("Anmelden");
@@ -56,7 +55,10 @@ public class LoginPrompt implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         if (loginField.readData().equals(loginPassword))
+        {
             controller.setAdministrationWindow();
+            loginFrame.dispose();
+        }
         else
             loginField.notifyError();
     }
