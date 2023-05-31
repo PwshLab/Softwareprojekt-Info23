cbba8e661e0e43fc6999ec161e9c2b50c3addb51
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-31 09:52:08 +0200

    Begin implementing TopBar with title and button

--- src/Controller/WindowSwitch/TopBar.java
+++ src/Controller/WindowSwitch/TopBar.java
@@ -1,7 +1,45 @@
 package src.Controller.WindowSwitch;
 
-import javax.swing.*;
+import src.Controller.Controller;
 
-public class TopBar extends JPanel
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TopBar extends JPanel implements ActionListener
 {
+    private final Controller controller;
+    private final boolean isNavigation;
+
+    public TopBar(Controller controller, boolean isNavigation)
+    {
+        this.controller = controller;
+        this.isNavigation = isNavigation;
+
+        initialize();
+    }
+
+    private void initialize()
+    {
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(1200, 25));
+        setMaximumSize(new Dimension(1200, 25));
+        JButton button;
+        if (isNavigation)
+            button = new JButton("Anmelden");
+        else
+            button = new JButton("Abmelden");
+        add(button, BorderLayout.EAST);
+        JLabel title = new JLabel("Search a Bar Software");
+        title.setFont(title.getFont().deriveFont(16f));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        add(title, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
 }
