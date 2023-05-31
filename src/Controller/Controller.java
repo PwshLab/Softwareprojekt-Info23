8bf4f184d04a9c501a65b84bc31cdfbb5d336b37
PwshLab <68289134+PwshLab@users.noreply.github.com>
2023-05-31 09:22:36 +0200

    Changed active panel in Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -80,7 +80,7 @@ public class Controller
         initializeMainWindow();
         initializeNavigationWindow();
         initializeAdministrationWindow();
-        //setWindowNavigation();
-        setAdministrationWindow();
+        setWindowNavigation();
+        //setAdministrationWindow();
     }
 }
